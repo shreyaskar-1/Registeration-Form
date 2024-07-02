a891618afaab7d2f4597a8789832adaeea74df0a
shreyaskar-1 <shreyaskar21@gmail.com>
2024-07-02 16:13:34 +0530

    Logout and Auth functionality

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require("express");
-const app = express();
-const Register = require("../src/models/registers");
 const path = require("path");
-require("./db/conn");
-const hbs = require('hbs');
+const cookieParser = require("cookie-parser");
+const hbs = require("hbs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 
+const Register = require("../src/models/registers");
+const auth = require("./middleware/auth");
+require("./db/conn");
 
+const app = express();
 const port = process.env.PORT || 3000;
 
+// Paths for static files, templates, and partials
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
@@ -25,95 +27,137 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Middleware to set authentication status
+// res.locals is an object in Express.js that provides a way to pass data from middleware to the view layer (templates) in your application. It exists throughout the lifecycle of the request-response cycle, making it a convenient place to store and access response-local variables.
+const setAuthStatus = (req, res, next) => {
+  res.locals.isAuthenticated = req.cookies.jwt ? true : false;
+  next();
+};
+
+app.use(setAuthStatus);
+
+// Routes
 app.get("/", (req, res) => {
-    res.render("index.hbs");
+  res.render("index.hbs");
 });
 
-app.get("/register", (req, res) => {
-    res.render("register.hbs");
-});
-
-// create new user
-app.post("/register", async (req, res) => {
-    try {
-        const password = req.body.password;
-        const cpassword = req.body.confirmpassword;
-
-        if (password === cpassword) {
-            const registerEmployee = new Register({
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                email: req.body.email,
-                gender: req.body.gender,
-                phone: req.body.phone,
-                age: req.body.age,
-                password: password,
-                confirmpassword: cpassword
-            });
-
-            // console.log("the success part" + registerEmployee)
-
-            const token = await registerEmployee.generateAuthToken();
-            // console.log("the token part" + token)
-
-            // The res.cookie() function is used to set the cookie name to value.
-            // The value parameter may be a string or object converted to json 
-            // res.cookie(name, value[options])
-
-            res.cookie("jwt",token,{
-                expires: new Date(Date.now() + 30000),
-                httpOnly:true
-            });
-
-            console.log(`this is cookie ${req.cookies.jwt}`);
-            
-            const registered = await registerEmployee.save();
-            res.status(201).render("index.hbs");
-        } else {
-            res.send("Passwords do not match");
-        }
-    } catch (e) {
-        res.status(400).send(e);
-    }
+app.get("/secret", auth, (req, res) => {
+  res.render("secret.hbs", { isAuthenticated: true });
 });
 
 app.get("/login", (req, res) => {
-    res.render("login.hbs");
+  res.render("login.hbs", { isAuthenticated: false });
 });
 
-// log in user
-app.post("/login", async (req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-        
-        // instance of the collection
-        const userEmail = await Register.findOne({ email });
+app.get("/register", (req, res) => {
+  res.render("register.hbs", { isAuthenticated: false });
+});
 
-        const isMatch = await bcrypt.compare(password, userEmail.password);
+app.post("/register", async (req, res) => {
+  try {
+    const { firstname, lastname, email, gender, phone, age, password, confirmpassword } = req.body;
 
-        const token = await userEmail.generateAuthToken();
-        // console.log("the token part " + token)
+    if (password === confirmpassword) {
+      const registerEmployee = new Register({
+        firstname,
+        lastname,
+        email,
+        gender,
+        phone,
+        age,
+        password,
+        confirmpassword
+      });
 
-        res.cookie("jwt",token,{
-            expires: new Date(Date.now() + 60000),
-            httpOnly:true
-        });
+      const token = await registerEmployee.generateAuthToken();
 
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 30000),
+        httpOnly: true
+      });
 
-
-        if (isMatch) {
-            res.status(201).render("index");
-        } else {
-            res.send("Invalid Password");
-        }
-    } catch (err) {
-        res.status(400).send("Invalid login details");
+      await registerEmployee.save();
+      res.status(201).render("index.hbs", { isAuthenticated: true });
+    } else {
+      res.send("Passwords do not match");
     }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+// app.post("/register", async (req, res) => {
+//     try {
+//       const password = req.body.password;
+//       const cpassword = req.body.confirmpassword;
+  
+//       if (password === cpassword) {
+//         const registerEmployee = new Register({
+//           firstname: req.body.firstname,
+//           lastname: req.body.lastname,
+//           email: req.body.email,
+//           gender: req.body.gender,
+//           phone: req.body.phone,
+//           age: req.body.age,
+//           password: password,
+//           confirmpassword: cpassword
+//         });
+  
+//         const token = await registerEmployee.generateAuthToken();
+  
+//         res.cookie("jwt", token, {
+//           expires: new Date(Date.now() + 30000),
+//           httpOnly: true
+//         });
+  
+//         console.log(`this is cookie ${req.cookies.jwt}`);
+        
+//         const registered = await registerEmployee.save();
+//         res.status(201).render("index.hbs", { isAuthenticated: true });
+//       } else {
+//         res.send("Passwords do not match");
+//       }
+//     } catch (e) {
+//       res.status(400).send(e);
+//     }
+//   });
+  
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userEmail = await Register.findOne({ email });
+
+    if (userEmail && await bcrypt.compare(password, userEmail.password)) {
+      const token = await userEmail.generateAuthToken();
+
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 60000),
+        httpOnly: true
+      });
+
+      res.status(201).render("index.hbs", { isAuthenticated: true });
+    } else {
+      res.send("Invalid email or password");
+    }
+  } catch (err) {
+    res.status(400).send("Invalid login details");
+  }
 });
 
+app.get("/logout", auth, async (req, res) => {
+  try {
+    res.clearCookie("jwt");
+    console.log("Logout successful");
 
+    if (req.user && typeof req.user.save === 'function') {
+      await req.user.save();
+    }
+    res.render("login.hbs", { isAuthenticated: false });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
